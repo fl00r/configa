8829e554113d5d9a6b92de9477fd9783708bb40a
fl00r <fl00r@yandex.ru>
2012-09-05 13:46:30 +0400

    Multifile env

--- lib/configa.rb
+++ lib/configa.rb
@@ -5,40 +5,63 @@ module Configa
   extend self
 
   def new(path)
-    yaml = load_yaml(path)
-    to_magickstruct(yaml)
-  end
-
-  def load_yaml(path)
-    file = File.read(path)
-    yaml = YAML.load(file)
-    root_keys = yaml.keys
-    merger = Proc.new do |data|
-      data.each do |k,v|
-        next  unless Hash === v
-        if root_keys.include? k
-          data[k.to_s] = yaml[k].merge(data[k])
-        else
-          merger.call(data)
-        end
-      end
-    end
-    yaml.each do |k,v|
-      merger.call(v)
-    end
-    yaml
-  end
-
-  def to_magickstruct(yaml)
-    config = MagickStruct.new(yaml).data
+    MagickStruct.new(path).data
   end
 
   class MagickStruct
     attr_reader :data
 
-    def initialize(data)
-      data = magick(data)
-      @data = data
+    def initialize(path)
+      @base_path = path
+      @base_dir = File.dirname(path)
+      load_and_parse_yaml
+    end
+
+    def load_and_parse_yaml(env=nil)
+      @yaml ||= begin
+        file = File.read(@base_path)
+        YAML.load(file)
+      end
+      if env
+        env_path = File.join(@base_dir, env.to_s + ".yml")
+        return nil  unless File.exist?(env_path)
+        file = File.read(env_path)
+        env_yaml = { "#{env}" => YAML.load(file) }
+        @yaml.merge!(env_yaml)
+      end
+      root_keys = @yaml.keys
+      merger = Proc.new do |data|
+        data.each do |k,v|
+          next  unless Hash === v
+          if root_keys.include? k
+            data[k.to_s] = @yaml[k].merge(data[k])
+          else
+            merger.call(data)
+          end
+        end
+      end
+      @yaml.each do |k,v|
+        merger.call(v)
+      end
+      @data = magick(@yaml)
+
+      struct = self
+      @data.define_singleton_method(:__struct) do
+        return struct
+      end
+
+      @data.define_singleton_method(:method_missing) do |name, *args, &blk|
+        if new_data = __struct.load_and_parse_yaml(name) 
+          @data.define_singleton_method(name) do
+            new_data[name]
+          end
+          new_data.send(name)
+        else
+          super(name, args, &blk)
+        end
+      end
+
+      @data
     end
 
     def magick(data)
--- spec/configa/configa_spec.rb
+++ spec/configa/configa_spec.rb
@@ -10,6 +10,23 @@ describe Configa do
 
     it "should parse simple yml" do
       @config.development.mysql.database.must_equal "mysql_dev"
+      @config.development(:mysql).must_equal([{"adapter"=>"mysql", "encoding"=>"utf8", "host"=>"localhost", "username"=>"root", "database"=>"mysql_dev"}])
+      @config.development.mysql.username.must_equal "root"
+      @config.production.mysql.username.must_equal "admin"
+      @config.production.mysql(:username, :database).must_equal ["admin", "mysql_prod"]
+      @config.development.mysql.must_equal({"database"=>"mysql_dev", "adapter"=>"mysql", "encoding"=>"utf8", "host"=>"localhost", "username"=>"root"})
+    end
+  end
+
+  describe "multiple file" do
+    before do
+      path = File.expand_path("../../base.yml", __FILE__)
+      @config = Configa.new(path)
+    end
+
+    it "should parse simple yml" do
+      @config.development.mysql.database.must_equal "mysql_dev"
+      @config.development(:mysql).must_equal([{"adapter"=>"mysql", "encoding"=>"utf8", "host"=>"localhost", "username"=>"root", "database"=>"mysql_dev"}])
       @config.development.mysql.username.must_equal "root"
       @config.production.mysql.username.must_equal "admin"
       @config.production.mysql(:username, :database).must_equal ["admin", "mysql_prod"]
